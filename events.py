75f483810da6d96ad47dcbec2ea0268dc6b78898
psyjm22 <psyjm22@nottingham.ac.uk>
2022-05-30 20:32:59 +0100

    Random kill messages (they work)

--- events.py
+++ events.py
@@ -1,16 +1,27 @@
 import random
 
 def kill(tributes):
+    
     trib1 = random.choice(tributes)
     tributes.remove(trib1)
     
     trib2 = random.choice(tributes)
     tributes.remove(trib2)
     
-    print(f"{trib1.name} kills {trib2.name}")
+    modesOfKill = [f"{trib1.name} kills {trib2.name}",
+                   f"{trib1.name} breaks {trib2.name}'s neck in a fight",
+                   f"{trib2.name} is killed by {trib1.name}",
+                   f"{trib1.name} gets in a brawl with {trib2.name}. {trib1.name} wins",
+                   f"{trib1.name} gets in a brawl with {trib2.name}. {trib2.name} wins",
+                   f"{trib1.name} pushes {trib2.name} off a cliff",
+                   
+                   ]
+    
+    print(random.choice(modesOfKill))
     
     tributes.append(trib1)
     trib1.playerKill()
+    trib2.setDead()
     
     return tributes
 
